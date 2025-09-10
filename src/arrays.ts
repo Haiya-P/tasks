88aff3e13926eb307fa6b00761238fa90b09839d
haiyap <haiya_patel@yahoo.com>
2025-09-10 12:18:20 -0400

    Finished BookEndList

--- src/arrays.ts
+++ src/arrays.ts
@@ -5,7 +5,12 @@
  * the number twice.
  */
 export function bookEndList(numbers: number[]): number[] {
-    return [];
+    let c =
+        numbers.length == 0 ? []
+        : numbers.length == 1 ? [numbers[0], numbers[0]]
+        : numbers.length >= 2 ? [numbers[0], numbers[numbers.length - 1]]
+        : [];
+    return c;
 }
 
 /**
