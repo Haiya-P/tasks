61d7e2add256d950b860252161bbcb20eb309f46
Haiya Patel <161632452+Haiya-P@users.noreply.github.com>
2025-10-02 20:38:28 -0400

    Clean up App component by removing unused elements
    Removed unnecessary elements and a button from the App component.

--- src/App.tsx
+++ src/App.tsx
@@ -19,8 +19,7 @@ function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                <div style={{ backgroundColor: "red" }}></div>
-                This is UD CISC275 with React Hooks and TypeScript
+                UD CISC275 with React Hooks and TypeScript
             </header>
             <hr></hr>
             {<DoubleHalf></DoubleHalf>}
@@ -55,21 +54,6 @@ function App(): React.JSX.Element {
             <hr />
             <ChangeColor></ChangeColor>
             <hr />
-            <h1> This is a header</h1>
-            <img src={IMG_2775} alt="My ugly bird :>" />
-            <p>Hello World! Haiya Patel</p>
-            <ul>
-                <li>I like Birds</li>
-                <li>I like Gundam</li>
-                <li>I hate Coding</li>
-            </ul>
-            <Button
-                onClick={() => {
-                    console.log("Hello World!");
-                }}
-            >
-                Log Hello World
-            </Button>
         </div>
     );
 }
